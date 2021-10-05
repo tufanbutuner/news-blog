e4b6e921d4531563375668530ff6cb4d457df92d
tufanbutuner <tufanbutuner@gmail.com>
2021-10-05 21:54:08 +0100

    Changed PostCard to TSX

--- components/molecules/PostCard/PostCard.tsx
+++ components/molecules/PostCard/PostCard.tsx
@@ -1,28 +1,27 @@
+import { ContentInfo, PostCardStyle } from "./styles";
+
 import Image from "next/image";
 import Link from "next/link";
-import { PostCardStyle } from "./styles";
+import React from "react";
 
 export default function PostCard({ post }) {
   const { title, slug, thumbnail, author, categories } = post.fields;
   return (
     <PostCardStyle>
-      <div className="thumbnail">
+      <ContentInfo>
         <Image
           src={`https:${thumbnail.fields.file.url}`}
           width={thumbnail.fields.file.details.image.width}
           height={thumbnail.fields.file.details.image.height}
         />
-      </div>
-      <div className="content-info">
-        <div className="info">
-          <h2>{title}</h2>
-          <p>By {author}</p>
-          <span>{categories}</span>
-          <div className="actions">
-            <Link href={`/posts/${slug}`}>Read more</Link>
-          </div>
+
+        <h3>{title}</h3>
+        <p>{author}</p>
+        <span>{categories}</span>
+        <div className="actions">
+          <Link href={`/posts/${slug}`}>Read more</Link>
         </div>
-      </div>
+      </ContentInfo>
     </PostCardStyle>
   );
 }
